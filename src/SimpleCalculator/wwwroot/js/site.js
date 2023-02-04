05ec008819282e8aefb08490f002bb62ce1c3a02
Rosen Spiridonov <rspiridonov3@gmail.com>
2023-02-04 15:36:07 +0200

    Add front-end evaluation

--- src/SimpleCalculator/wwwroot/js/site.js
+++ src/SimpleCalculator/wwwroot/js/site.js
@@ -1,4 +1,43 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.onload = function () {
+    const calculatorForm = document.querySelector('.calculator');
 
-// Write your JavaScript code.
+    calculatorForm.addEventListener('submit', function (e) {
+        const checkbox = e.target.querySelector('#js-eval');
+
+        if (checkbox.checked) {
+            e.preventDefault();
+            const expression = e.target.querySelector('#expression').value;
+            const resultEl = e.target.querySelector('.result');
+            resultEl.textContent = '';
+            clearErrors(e.target);
+
+            if (!isExpressionValid(expression)) {
+                showError(e.target);
+                return;
+            }
+
+            let result;
+            try {
+                result = eval(expression);
+            } catch {
+                showError(e.target);
+            }
+            resultEl.textContent = result;
+        }
+    });
+
+    function isExpressionValid(expression) {
+        const pattern = /^[\d\+\-\*\/\(\)\.\s]+$/;
+        return pattern.test(expression);
+    }
+
+    function showError(form) {
+        const error = document.createElement('span');
+        error.textContent = "Invalid expression.";
+        form.querySelector('.errors').appendChild(error);
+    }
+
+    function clearErrors(form) {
+        form.querySelector('.errors').innerHTML = '';
+    }
+};
