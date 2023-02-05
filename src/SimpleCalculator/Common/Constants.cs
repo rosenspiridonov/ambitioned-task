15949982f8950215d6f1dc849d87704a4ff66531
Rosen Spiridonov <rspiridonov3@gmail.com>
2023-02-05 11:49:40 +0200

    Final polishing

--- src/SimpleCalculator/Common/Constants.cs
+++ src/SimpleCalculator/Common/Constants.cs
@@ -17,7 +17,7 @@
             public const string InvalidExpression = "Invalid expression.";
             public const string DivisionByZero = "Division by zero is not allowed.";
             public const string InvalidOperator = "Invalid operator: {0}.";
-            public const string InvalidTokens = "Allowed characters are digits and +, -, *, /, (, ) and .";
+            public const string InvalidTokens = "Allowed characters are digits, +, -, *, /, (, ) and .";
             public const string ExpressionEmpty = "Expression cannot be empty.";
         }
     }
